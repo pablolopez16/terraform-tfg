862c9c4fa916b9b76cc9b1c89418f4d44450b4da
Pablo Lopez <pablolopez161616@gmail.com>
2026-04-18 16:50:08 +0200

    cambios shaded , e incoporacion de llamadas terraform

--- main.tf
+++ main.tf
@@ -1,21 +1,27 @@
 //Creacion Lambda
-/*resource "aws_lambda_function" "aws-lambda-tfg" {
+resource "aws_lambda_function" "aws-lambda-tfg" {
     function_name = "aws-lambda-tfg"
     role = data.aws_iam_role.lambda_role.arn
     runtime = "java17"
     handler = "tfg.prod.Main::handleRequest"
 
-    filename = "C:/TFG/aws-lambda-01/target/lambda-1.0.0-shaded.jar"
-    source_code_hash = filebase64sha256("C:/TFG/aws-lambda-01/target/lambda-1.0.0-shaded.jar")
-  
-}*/
-
-data "aws_lambda_function" "aws-lambda-tf-01" {
-    function_name = "aws-lambda-tfg"
+    s3_bucket = aws_s3_bucket.aws-lambda-tfg-bucket.bucket
+    s3_key = aws_s3_object.lambda_jar.key
+    source_code_hash = aws_s3_object.lambda_jar.etag /*para detectar cambios automaticamente*/
+    timeout = 30
 }
+resource "aws_s3_object" "lambda_jar" {
+    bucket = aws_s3_bucket.aws-lambda-tfg-bucket.bucket
+    key    = "tfg/lambda_function_code/lambda-1.0.0.jar"
+    source = var.lambda_jar_path    
+}
+
 resource "aws_s3_bucket" "aws-lambda-tfg-bucket" {
     bucket = "aws-tfg-bucket-plfz"
     tags = {
         "Name" = "state-file"
     }
+
+#region API GateAway
+
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "lambda_jar_path"{
+    description = "Path to the lambda jar file"
+    type        = string
+    default     = "C:/TFG/aws-lambda-01/target/lambda-1.0.0.jar"
+}
