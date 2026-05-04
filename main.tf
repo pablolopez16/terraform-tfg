95f8fd8b034617cf05dff3ac02d6497f08cafc8e
Pablo Lopez <pablolopez161616@gmail.com>
2026-05-04 21:27:45 +0200

    camiso frontend y cambio de cors por frontend

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_apigatewayv2_api" "api-gateway-tfg" {
   protocol_type = "HTTP"
 
    cors_configuration {
-    allow_origins = ["*"]   # cambiar a la URL del S3 frontend cuando esté listo
+    allow_origins = ["http://aws-tfg-frontend-plfz.s3-website-us-east-1.amazonaws.com"]   # cambiar a la URL del S3 frontend cuando esté listo
     allow_methods = ["GET", "POST", "DELETE", "OPTIONS"]
     allow_headers = ["Content-Type", "Authorization"]
     max_age       = 300
